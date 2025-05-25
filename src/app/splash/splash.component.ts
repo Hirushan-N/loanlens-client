cfe27acb0bec18ef9b32ff9fec7206be3887efc6
Hirushan-N <78243763+Hirushan-N@users.noreply.github.com>
2025-05-25 12:53:32 +0530

    Update splash.component.ts

--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 
@@ -11,6 +11,12 @@ import { Router } from '@angular/router';
 })
 export class SplashComponent {
   constructor(private router: Router) {}
+
+  @HostListener('document:keydown.enter', ['$event'])
+  handleEnterPress(event: KeyboardEvent): void {
+    event.preventDefault(); // optional
+    this.navigateToForm();
+  }
 
   navigateToForm(): void {
     const splash = document.querySelector('.splash-container');
